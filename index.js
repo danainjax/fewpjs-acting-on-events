7bd5b0daac34d6fa6d6bb8bd50c3ed4a48318754
danainjax <danainjax@gmail.com>
2021-05-19 15:12:20 -0400

    done

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 
-//test comment
-//second comment
+let dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = "#FF69B4";
+// document.addEventListener("keydown", function(e) {
+//   if (e.key === "ArrowLeft") {
+//     let leftNumbers = dodger.style.left.replace("px", "");
+//     let left = parseInt(leftNumbers, 10);
+
+//     dodger.style.left = `${left - 1}px`;
+//   }
+// }); 
+
+//moves dodger left, by reading the left keydown event
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+} 
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+}); 
+//same as the other function except going the other direction and listening for the right keydown
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+}); 
